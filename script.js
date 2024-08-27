dab7918cc71c45b676f883828c30399ee5e739c8
William Ritchie <williamrobertritchie@gmail.com>
2024-08-27 16:34:16 -0400

    webpacking stuff

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+import { initializeApp } from 'firebase/app';
+//import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAegBd_qulAcOvbIbjOeQtyzQw5dygKEX4",
+  authDomain: "solartest-7875a.firebaseapp.com",
+  databaseURL: "https://solartest-7875a-default-rtdb.firebaseio.com",
+  projectId: "solartest-7875a",
+  storageBucket: "solartest-7875a.appspot.com",
+  messagingSenderId: "510830787909",
+  appId: "1:510830787909:web:05406f7adb94f069ded22c"
+};
+
+initializeApp(firebaseConfig);
+
+//const database = getDatabase();
+
 var clickedTile = document.getElementById("p1");
+
+Coloris({
+  alpha: false
+});
+
 
 function tileClicked(event) {
 	//clickedTile = document.getElementById("p1");
-	console.log(clickedTile);
+	//console.log(clickedTile);
 	clickedTile.style.backgroundColor = null;
 	clickedTile.style.transform = 'scale(1)';
 
