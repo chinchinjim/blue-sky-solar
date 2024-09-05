af008e86d4a1f197e3de57b02ffc67f6887a2189
William Ritchie <williamrobertritchie@gmail.com>
2024-09-05 14:27:13 -0400

    Text updates with cell adoption. Removed ability to adopt cell more than once

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFunctions, httpsCallable } from "firebase/functions";
-import { getDatabase, ref, get, onChildAdded } from 'firebase/database';
+import { getDatabase, ref, onChildAdded, get, query, orderByChild, equalTo } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAegBd_qulAcOvbIbjOeQtyzQw5dygKEX4",
@@ -21,39 +21,46 @@ const cellRef = ref(database, "/cells");
 
 var cachedCells = {};
 
+// Holds currently selected tile
+var clickedTile = document.getElementsByClassName("outer")[0];
+
 // Event listener - Listens to added cells and updates realtime
-// TODO: 1. Remove cache, implement uses of cache with firebase's once() function
 onChildAdded(cellRef, (cellData) => {
 	// Data of adopted cell
 	let val = cellData.val();
-	// Cache the cell (Should be removed and use firebase once() or get())
+	// Cache the cell
 	cachedCells[val["cell"]] = {"name": val["name"], "message": val["message"], "color": val["color"]}
 	//Update color of the cell
-	document.getElementById(val.cell).style.backgroundColor = val.color;
+	document.getElementById(val.cell).style.backgroundColor = val["color"];
+	if(clickedTile.id == val.cell) {
+		document.getElementById("cell-name").innerHTML = val["name"];
+		document.getElementById("cell-message").innerHTML = val["message"];
+	}
 });
 
-// Holds currently selected tile
-var clickedTile = document.getElementById("p1");
-
 // Tile selector
-// TODO: 1. Unadopted cells need a default color and a selected color
-// 	 2. Check database for if the cell is adopted instead of checking if there is a color
 document.getElementsByClassName("outer")[0].onclick = (event) => {
 
+	//Check cache to see if cell is adopted
+	let adopted = clickedTile.id in cachedCells;
+
 	// Previously selected tile is set back to normal
-	clickedTile.style.backgroundColor = clickedTile.style.backgroundColor == "white" ? "" : clickedTile.style.backgroundColor;
+	clickedTile.style.backgroundColor = adopted? clickedTile.style.backgroundColor : "";
 	clickedTile.style.transform = 'scale(1)';
 
 	// Selects a new tile if the target element clicked is a tile
 	clickedTile = event.target.className == "cell" ? event.target : clickedTile;
+
+	adopted = clickedTile.id in cachedCells;
 	
 	// Modifies the selected cell to show user it is selected
-	clickedTile.style.backgroundColor = clickedTile.style.backgroundColor == "" ? "white": clickedTile.style.backgroundColor;
+	clickedTile.style.backgroundColor = adopted? clickedTile.style.backgroundColor : "white";
 	clickedTile.style.transform = 'scale(1.2)';
 
 	// Updates text fields with cell information 
 	document.getElementById("cell-id").innerHTML = "Cell #" + clickedTile.id.substring(1);
-	if(clickedTile.id in cachedCells) {
+	
+	if(adopted) {
 		document.getElementById("cell-name").innerHTML = cachedCells[clickedTile.id]["name"];
 		document.getElementById("cell-message").innerHTML = cachedCells[clickedTile.id]["message"];
 	} else {
@@ -63,20 +70,22 @@ document.getElementsByClassName("outer")[0].onclick = (event) => {
 }
 
 // Adoption form
-// TODO: 1. Prevent adoption of adopted cells
-// 	 2. Integrate payment stuff
-document.getElementById("adopt-form-submit").onclick = async () => {
-	
+// TODO - Integrate payment stuff 
+document.getElementById("adopt-form-submit").onclick = () => {
 	// Takes user input and cell id to make JSON object
 	var cellData = {"cell": clickedTile.id};
 	for (const input of document.getElementById("adopt-form").querySelectorAll('input')) {
 		cellData[input.name] = input.value;
 		input.value = "";
 	}
-	//once()
-	
-	// Calls backend to store the JSON object in the database
+
+	// Post info over to backend to write to database
 	const adoptCell = httpsCallable(functions, 'adopt_cell');
-	adoptCell(cellData);
+	adoptCell(cellData).then((res) => {
+		// Cell is adopted already if response isn't "Success"
+		if(res.data != "Success") {
+			alert(res.data);
+		}
+	});
 };
 
